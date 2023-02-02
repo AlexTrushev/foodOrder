2889e355ddc7154f0db28207cf3f66f1679fa0ac
Alex Trushev <alex.trushev95@gmail.com>
2023-02-03 00:50:04 +0100

    Add modal for Cart

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,3 +1,35 @@
-const Modal = (props) => {};
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const PortalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={props.onClick} />,
+        PortalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay onClick={props.onClick}>{props.children}</ModalOverlay>,
+        PortalElement
+      )}
+    </Fragment>
+  );
+};
 
 export default Modal;
